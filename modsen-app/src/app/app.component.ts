bf2de87162cea809940cf5d98006662de702ffb5
Olga Zhurova <82470486+ozhurova@users.noreply.github.com>
2022-01-26 17:39:37 +0300

    installed ngx translate

--- modsen-app/src/app/app.component.ts
+++ modsen-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'modsen-app';
+  constructor(
+    public translate: TranslateService
+  ) {
+    translate.addLangs(['en', 'ru']);
+    translate.setDefaultLang('ru');
+  }
+  switchLang(lang: string) {
+    this.translate.use(lang);
+  }
 }
--- modsen-app/src/app/app.module.ts
+++ modsen-app/src/app/app.module.ts
@@ -8,6 +8,9 @@ import {HomePageModule} from "./pages/home-page/home-page.module";
 import {HeaderModule} from "./components/header/header.module";
 import {FooterModule} from "./components/footer/footer.module";
 import {ChooseTestsPageModule} from "./pages/choose-tests-page/choose-tests-page.module";
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,10 +23,21 @@ import {ChooseTestsPageModule} from "./pages/choose-tests-page/choose-tests-page
     HomePageModule,
     HeaderModule,
     FooterModule,
-    ChooseTestsPageModule
+    ChooseTestsPageModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+}
+export function httpTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http);
 }
