443b145afba7cc4cd75f68af48c74b436996568f
Olga Zhurova <82470486+ozhurova@users.noreply.github.com>
2022-02-10 10:30:47 +0300

    added card to home page and fixed names for translate

--- modsen-app/src/app/pages/home-page/home-page.module.ts
+++ modsen-app/src/app/pages/home-page/home-page.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
 import { RouterModule } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  declarations: [HomePageComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [HomePageComponent, CardComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatCardModule,
+    MatButtonModule,
+    TranslateModule,
+  ],
   exports: [HomePageComponent],
 })
 export class HomePageModule {}
